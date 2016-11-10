0cbe67ed441040bb65c8a263c7554408f14fa73b
Antoh1 <antotiro@gmail.com>
2016-11-10 22:40:36 +0300

    updated the test cases

--- test/test_prime.py
+++ test/test_prime.py
@@ -8,15 +8,15 @@ class primeNumberTestCase(unittest.TestCase):
 	def tes_if_output_is_list(self):
 		self.assertIsInstance(prime.prime_numbers(14), list)	
 	def test_if_number_is_one(self):
-		self.assertEqual(prime.prime_numbers(1),"a prime number is greater than one")
+		self.assertEqual(prime.prime_numbers(1),"prime numbers are greater than one")
 	def test_if_number_is_zero(self):
-		self.assertEqual(prime.prime_numbers(0),"a prime number is greater than one")
+		self.assertEqual(prime.prime_numbers(0),"prime numbers are greater than one")
 	def test_if_number_is_two(self):
 		self.assertEqual(prime.prime_numbers(2),[2])
 	def test_if_number_is_three_or_less(self):
 		self.assertEqual(prime.prime_numbers(3),[2,3])			
 	def test_less_than_zero(self):
-		self.assertEqual(prime.prime_numbers(-1),"a prime number is greater than one")
+		self.assertEqual(prime.prime_numbers(-1),"prime numbers are greater than one")
 	def test_number_type_not_string(self):
 		self.assertEqual(prime.prime_numbers("string"),"Only integers allowed")
 	def test_number_type_not_list(self):

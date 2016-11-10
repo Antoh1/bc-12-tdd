d9191f970fc853e64dfad5b2824b71aefad1884d
Antoh1 <antotiro@gmail.com>
2016-11-10 17:45:02 +0300

    first commit

---
+++ app/prime.py
@@ -0,0 +1,20 @@
+def prime_no(number):
+	"""this function checks for prime numbers in a range and outputs them in a list"""
+	if number==1 or number==0:
+		return "prime number is greater than one"
+	if number<0:
+		return "No negative prime numbers"
+	if number==2:
+		return [2]	
+	primes = [2,3]
+	if number<=3:
+		return primes
+	for num in range(4,number+1):
+		p_flag = True
+		for value in primes:
+			if num%value==0:
+				p_flag = False
+		if p_flag:
+			primes.append(num)        	
+	return primes					 
+print(prime_no(23))
---
+++ test/test_prime_no.py
@@ -0,0 +1,14 @@
+from app import prime
+
+import unittest
+
+class primeNumberTestCase(unittest.TestCase):
+	def test_if_number_passed_is_prime(self):
+		self.assertEqual(prime.prime_no(7), [2,3,5])
+	def test_if_number_less_than_2(self):
+		self.assertEqual(prime.prime_no(1),"prime number is greater than one")
+	def test_if_number_less_than_2(self):
+		self.assertEqual(prime.prime_no(0),"prime number is greater than one")	
+	def test_less_than_zero(self):
+		self.assertEqual(prime.prime_no(-1),"No negative prime numbers")			
+
